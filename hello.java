a6a149da40f3c96f5bfd44658af66f126e6f0899
Vaishnavi-p44 <rajakumarpawar4@gmail.com>
2026-04-28 23:49:20 +0530

    Initial commit - Java basics

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello {
+    public static void main(String[] args) {
+        int x=3;
+        int y=4;
+        int r=9;
+        int g=8;
+        boolean result= x<y&&r>g;
+        System.out.println(result);
+    }
+    
+}
---
+++ literals.java
@@ -0,0 +1,15 @@
+public class literals {
+    public static void main(String[] args){
+      
+       // write code here
+       System.out.println("Addition: 30");
+       System.out.println("Subtraction: 10");
+       System.out.println("Multiplication: 200");
+       System.out.println("Division: 2");
+       System.out.println("Is a greater than b? true");
+       System.out.println("Is a>b and b>O? true");
+       
+       
+  }
+}
+
