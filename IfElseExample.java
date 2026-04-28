a1b854a3d400ebb6f53f5a0250c16584b290be18
Vaishnavi-p44 <rajakumarpawar4@gmail.com>
2026-04-29 00:23:14 +0530

    Day 2 - if-else program

---
+++ IfElseExample.java
@@ -0,0 +1,12 @@
+    public class IfElseExample {
+    public static void main(String[] args) {
+        int marks = 75;
+
+        if (marks >= 50) {
+            System.out.println("Passed");
+        } else {
+            System.out.println("Failed");
+        }
+    }
+}
+    
